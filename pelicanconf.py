423ce8afa714623a73a9b132663a3626891f12ab
Marc Garcia <marc.garcia1@tesco.com>
2018-09-07 17:43:15 +0100

    Adding draft of new pelican blog

---
+++ pelicanconf.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+AUTHOR = 'Marc Garcia'
+SITENAME = 'datapythonista blog'
+SITEURL = '/blog'
+OUTPUT_PATH = 'blog'
+PATH = 'content'
+TIMEZONE = 'Europe/London'
+DEFAULT_LANG = 'English'
+THEME = 'attila'
+STATIC_PATHS = ['static']
+HEADER_COVER = 'static/bg.jpg'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+DEFAULT_PAGINATION = False
